aefe3455c602ebfc9079d1b1dc815ae6eaa4d659
michielstock <michielfmstock@gmail.com>
2020-08-26 15:46:23 +0200

    :tada: ordinal SVM

--- src/ordinalregr.jl
+++ src/ordinalregr.jl
@@ -1,4 +1,4 @@
-module OrdinalSVM
+module OrdinalRegr
 
 using Convex, SCS
 
@@ -12,15 +12,47 @@ function fitordinalsvm(y, X, L; C=1e-2)
     n, p = size(X)
     l = length(L)
     w = Variable(p)
-    b = Variable(l)
+    b = Variable(l - 1)
     # get indexes of the labels
-    inds = indexin(y, L)
+    inds = convert(Vector{Int}, indexin(y, L))
     # compute objective
-    obj = sum(max(0, 1 - X * w  - b[inds])) + C * sum(w .* w)
-    problem = minimize(obj, b[1]==0.0, (b[i] <= b[i+1] for i in 1:l-1)...)
+    obj = C * sum(w .* w)
+    f = X * w
+    for (j, lj) in enumerate(L[1:end-1])
+        for (i, yi) in enumerate(y)
+            obj += yi ≤ lj ? max(0.0, 1.0 + (f[i] - b[j])) : max(0.0, 1.0 - (f[i] - b[j]))
+        end
+    end
+    problem = minimize(obj, (b[i] <= b[i+1] for i in 1:l-2)...)
     solve!(problem, () -> SCS.Optimizer(verbose=false))
+    return OrdinalSVM(w.value[:], b.value[:], L)
+end
 
-    return OrdinalSVM(w.value, b.value, L)
+fitordinalsvm(y, X; C=1e-2) = fitordinalsvm(y, X, sort!(unique(y)); C=1e-2)
+
+"""
+    score(model::OrdinalSVM, X::AbstractMatrix)
+
+Compute the scoring using an `OrdinalSVM` model.
+"""
+function score(model::OrdinalSVM, X::AbstractMatrix)
+    X * model.w
+end
+
+"""
+predict(model::OrdinalSVM, X::AbstractMatrix)
+
+Predict the label using an `OrdinalSVM` model.
+"""
+function predict(model::OrdinalSVM, X::AbstractMatrix)
+    l = [model.L[1] for i in 1:size(X, 1)]
+    for (i, s) in enumerate(score(model, X))
+        for (Lj, bj) in zip(model.L[2:end], model.b)
+            s - bj ≥ 0 && (l[i] = Lj)
+        end
+    end
+    return l
+
 end
 
 
--- test/ordinalregr.jl
+++ test/ordinalregr.jl
@@ -1,7 +1,16 @@
 @testset "ordinal SVM" begin
+
+using BioLizard: OrdinalRegr
     
 L = [:A, :B, :C]
 
-y = []
+X = randn(100, 5)
+
+y = [xi + 0.1randn() < -2 ? :A : (xi + 0.1randn() < 0 ? :B : :C) for xi in X[:,4]]
+
+model = fitordinalsvm(y, X, L)
+
+@test predict(model, X) isa Vector{Symbol}
+@test score(model, X) isa Vector{Float64}
 
 end
